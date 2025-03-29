664dfd740071c10ed569ab2a51c3aca2334214cc
gmlotfi <genavivelotfi@yahoo.com>
2025-03-29 17:11:51 -0400

    idek

--- posts/forms.py
+++ posts/forms.py
@@ -6,17 +6,19 @@ class PostForm(forms.ModelForm):
         model = Post
         #fields = ['title', 'content']
         fields = ['title', 'content', 'image']
-        widgets = {
+
+        
+        """widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Write your post...'}),
             'image': forms.ClearableFileInput(attrs={'class': 'form-control-file', 'accept': 'image/*'})
-        }
+        }"""
     
-    def clean_image(self):
+    '''def clean_image(self):
         image = self.cleaned_data.get('image')
         if not image:
             raise forms.ValidationError("An image is required for the post.")
         if image.content_type not in ['image/jpeg', 'image/png', 'image/gif']:
             raise forms.ValidationError("Only JPEG, PNG, and GIF images are allowed.")
-        return image
+        return image'''
 
