27305395e7a50505b5293fe25f011eaf010e6290
gmlotfi <genavivelotfi@yahoo.com>
2025-03-29 13:16:26 -0400

    added a my post/ display my posts function

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create_post, name='create_post'),
+    path('my-posts/', views.user_posts, name='user_posts'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -16,3 +16,10 @@ def create_post(request):
     else:
         form = PostForm()
     return render(request, 'posts/create_post.html', {'form': form})
+
+@login_required
+def user_posts(request):
+    # Get all posts created by the logged-in user
+    posts = Post.objects.filter(author=request.user)
+    
+    return render(request, 'posts/user_posts.html', {'posts': posts})
