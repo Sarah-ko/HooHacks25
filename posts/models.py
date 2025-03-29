ffd833185fa32abaf58b6633038f8f8be0f7271b
gmlotfi <genavivelotfi@yahoo.com>
2025-03-29 13:11:21 -0400

    created prelim posts model, view, url

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    file = models.FileField(upload_to='posts/files/', blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #will need to change user to whatever auth is called
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
+from .models import Post
 
 # Create your views here.
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'posts/create_post.html', {'form': form})
